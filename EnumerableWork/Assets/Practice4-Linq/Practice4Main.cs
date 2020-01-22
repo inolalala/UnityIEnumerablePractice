43d51a34429a94f229329a823b9668bd249e23ba
InoueSota <asitanobento87@gmail.com>
2020-01-22 23:19:24 +0900

    ナベアツコード

---
+++ EnumerableWork/Assets/Practice4-Linq/Practice4Main.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace Practice4
+{
+	public class Practice4Main : MonoBehaviour
+	{
+		
+		private void Start()
+		{
+			var enumerable = Enumerable.Range(0, 100).ToArray();
+			var sannobaisu = enumerable.Where(e => e % 3 == 0).ToList();
+			sannobaisu.ForEach(s => Debug.Log(s.ToString()+"なのでアホになっちゃう！"));
+		}
+	}
+}
