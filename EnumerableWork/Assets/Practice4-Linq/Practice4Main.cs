a74bb1be04b3dbf9562280c65122194875873ee8
InoueSota <asitanobento87@gmail.com>
2020-01-27 23:01:39 +0900

    Linqの練習(Where、Select、Skip、TakeWhile)

--- EnumerableWork/Assets/Practice4-Linq/Practice4Main.cs
+++ EnumerableWork/Assets/Practice4-Linq/Practice4Main.cs
@@ -11,8 +11,17 @@ namespace Practice4
 		private void Start()
 		{
 			var enumerable = Enumerable.Range(0, 100).ToArray();
-			var sannobaisu = enumerable.Where(e => e % 3 == 0).ToList();
-			sannobaisu.ForEach(s => Debug.Log(s.ToString()+"なのでアホになっちゃう！"));
+			//var sannobaisu = enumerable.Where(e => e % 3 == 0).ToList();
+			//sannobaisu.ForEach(s => Debug.Log(s.ToString()+"なのでアホになっちゃう！"));
+
+			//var sannonbaisunijyo = enumerable.Where(e => e % 3 == 0).Select(e => e * e).ToList();
+			//sannonbaisunijyo.ForEach(s => Debug.Log(s.ToString() + "なので2乗のアホになっちゃう！"));
+
+			//var sannonbaisunijyo = enumerable.Where(e => e % 3 == 0).Select(e => e * e).Skip(1).ToList();
+			//sannonbaisunijyo.ForEach(s => Debug.Log(s.ToString() + "なので2乗のアホになっちゃう！"));
+
+			var sanjyumadetoru = enumerable.TakeWhile(e => e <= 30).ToList();
+			sanjyumadetoru.ForEach(s => Debug.Log(s.ToString() + "は30より大きくない"));
 		}
 	}
 }
